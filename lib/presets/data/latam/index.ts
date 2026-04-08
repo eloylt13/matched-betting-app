047a8aa05b53555722d6e83a70f959edfcf19f29
eloylt13 <eloylte@gmail.com>
2026-04-08 18:20:32 +0200

    add latam presets index

---
+++ lib/presets/data/latam/index.ts
@@ -0,0 +1,19 @@
+import { casasReg } from './reg'
+import { casasCl } from './cl'
+import { casasPe } from './pe'
+import { casasEc } from './ec'
+import { casasMx } from './mx'
+import { casasAr } from './ar'
+import { casasPa } from './pa'
+import { casasUy } from './uy'
+
+export const casasLatam = [
+  ...casasReg,
+  ...casasCl,
+  ...casasPe,
+  ...casasEc,
+  ...casasMx,
+  ...casasAr,
+  ...casasPa,
+  ...casasUy,
+]
